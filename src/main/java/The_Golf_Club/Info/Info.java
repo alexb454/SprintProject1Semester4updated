75fcfa5996a3c140596bc8149837e949b8bbc609
alexb454 <71828880+alexb454@users.noreply.github.com>
2021-11-16 14:50:08 -0330

    new added files

--- src/main/java/The_Golf_Club/Info/Info.java
+++ src/main/java/The_Golf_Club/Info/Info.java
@@ -1,22 +1,19 @@
 package The_Golf_Club.Info;
 
 public class Info {
-    private int id;
-    private String Information;
+    private final long id;
+    private final String information;
 
-    public int getId() {
+    public Info(long id, String information){
+        this.id = id;
+        this.information = information;
+    }
+
+    public long getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getInformation() {
-        return Information;
-    }
-
-    public void setInformation(String information) {
-        Information = information;
+    public String getInformation(){
+        return information;
     }
 }
--- src/main/java/The_Golf_Club/Info/InfoController.java
+++ src/main/java/The_Golf_Club/Info/InfoController.java
@@ -8,14 +8,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
 @RestController
 public class InfoController {
     private static final String template = "Here is your membership info %s!";
-    //private final AtomicLong counter = new AtomicLong();
+    private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/info")
-    public Info info(@RequestParam(value = "Name", defaultValue = "Golf") String Name) {
-        return new Info();//(counter.incrementAndGet(), String.format(template, Name));
+    public Info info(@RequestParam(value = "name", defaultValue = "World") String Name) {
+        return new Info(counter.incrementAndGet(), String.format(template, Name));
     }
 
 }
